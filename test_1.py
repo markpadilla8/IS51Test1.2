aa2370843c5d34876f830a8c5dd6ed3ae28325ce
markpadilla8 <78052733+markpadilla8@users.noreply.github.com>
2021-04-15 13:50:25 -0700

    finish exam

--- test_1.py
+++ test_1.py
@@ -21,10 +21,11 @@ If the option2 is better, we output to the user "Option 2 is better"
 
 # option2
     amount = 1
-    list1 =[]
+    list1 = []
     loop 10 times
         add amount to list1
         amount *= 2
+    sum = sum of all items in loop
     return amount
 # main
     var1= option1
@@ -39,3 +40,36 @@ If the option2 is better, we output to the user "Option 2 is better"
 
 main
 """
+
+
+def option1():
+    return 100 * 10
+
+
+def option2():
+     amount = 1
+     list = []
+    for i in range(0,10):
+        list1.append(amount)
+        amount *= 2
+    print("list1", list1)
+    total = sum(list1)
+    return total
+
+def main(): 
+    answer = ""
+    var1 = option1() # 1000
+    var2 = option2() # 1023
+    print("from main:var1", var1, "var2", var2)
+    If var1 = var2:
+        answer = "Option 1 and Option 2 are the same" 
+    If var1 < var2:
+        answer = "Option 2 is better" 
+    else:
+        answer = "Option 1 is better"
+
+var1 = option1()
+var2 = option2()
+option2()
+print(var1)
+print(var2)
